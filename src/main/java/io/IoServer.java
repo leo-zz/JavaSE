bf16a16cc0e6044f42dec9c34a413a37e03013de
leo-zz <libing@zih718.com>
2019-01-28 18:42:52 +0800

    Adjust socket test example

--- src/main/java/io/IoServer.java
+++ src/main/java/io/IoServer.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,52 +27,55 @@ public class IoServer {
                 //accept方法时阻塞的
                 Socket socket = serverSocket.accept();
 
-
                 executorService.execute(() -> {
-                    //如何让socket一直工作？
-                    System.out.println(Thread.currentThread().getName() + "创建新的线程，处理Socket");
-                    byte[] bytes = new byte[1024];
-                    StringBuilder sb = new StringBuilder();
-                    OutputStream ops = null;
-                    InputStream ips = null;
-                    int num = 0;
-                    try {
-                        ips = socket.getInputStream();
-                        ops = socket.getOutputStream();
-                        while (true) {
-                            //read方法时阻塞的，确保Socket不关闭的话，可以一直使用这个连接通信
-//                            while ((num = ips.read(bytes)) != -1) {
-                            num = ips.read(bytes);
-                            if(num==-1){
-                                System.out.println("Socket通信结束");
-                                break;
-                            }
-                            sb.append(new String(bytes, 0, num));
-                            System.out.println(Thread.currentThread().getName() + "开始读取数据，长度:" + num);
-//                            }
-                            System.out.println(Thread.currentThread().getName() + "数据读取完毕，内容:" + sb.toString());
-
-                            ops.write((Thread.currentThread().getName() + "接受成功").getBytes());
-                            ops.flush();
-                            System.out.println(Thread.currentThread().getName() + "返回数据完成");
-                            //清空
-                            sb.delete(0,sb.length());
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            if (ops != null) ops.close();
-                            System.out.println("关闭链接");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    handleSocket(socket);
                 });
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private void handleSocket(Socket socket) {
+
+        //如何让socket一直工作？
+        System.out.println(Thread.currentThread().getName() + "创建新的线程，处理Socket");
+        byte[] bytes = new byte[1024];
+        StringBuilder sb = new StringBuilder();
+        OutputStream ops = null;
+        InputStream ips = null;
+        int num = 0;
+        try {
+            ips = socket.getInputStream();
+            ops = socket.getOutputStream();
+            while (true) {
+                //read方法时阻塞的，确保Socket不关闭的话，可以一直使用这个连接通信
+//                            while ((num = ips.read(bytes)) != -1) {
+                num = ips.read(bytes);
+                if (num == -1) {
+                    System.out.println("Socket通信结束");
+                    break;
+                }
+                sb.append(new String(bytes, 0, num));
+                System.out.println(Thread.currentThread().getName() + "开始读取数据，长度:" + num);
+//                            }
+                System.out.println(Thread.currentThread().getName() + "数据读取完毕，内容:" + sb.toString());
+
+                ops.write((Thread.currentThread().getName() + "接受成功").getBytes());
+                ops.flush();
+                System.out.println(Thread.currentThread().getName() + "返回数据完成");
+                //清空
+                sb.delete(0, sb.length());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ops != null) ops.close();
+                System.out.println("关闭链接");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
